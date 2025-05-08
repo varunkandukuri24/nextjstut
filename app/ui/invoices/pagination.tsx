cf973754515017fe8eb98432d875b023211d25ab
varunkandukuri24 <vkandukuri24@gmail.com>
2025-05-08 16:53:01 -0700

    implement invoice search with nextjs hooks

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -8,7 +8,7 @@ import { generatePagination } from '@/app/lib/utils';
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  //const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
